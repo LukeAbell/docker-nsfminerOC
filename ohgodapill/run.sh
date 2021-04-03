cc4e21a5bc11f7bbf5f420194546edcb14d4b490
Luke Abell <luke@abelltech.com>
2021-04-03 02:40:12 -0400

    Update run.sh

--- ohgodapill/run.sh
+++ ohgodapill/run.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 while true; do
-	#Sleep before running
-	sleep 15
-
-	/hive/opt/ohgodapill/OhGodAnETHlargementPill-r2 > /var/run/hive/ohgodapill 2>&1
+	/opt/ohgodapill/OhGodAnETHlargementPill-r2 2>&1
 
 	sleep 1
 done
